fa4fc32de768b808b876e70dd8c063d0445149f0
s-tama <stama15.dev@gmail.com>
2021-10-30 00:35:28 +0900

    敵の出現処理を追加

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{ 
+    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] float _appearSeconds;
+    Enemy _enemy;
+
+    public Action<Enemy> onAppear;
+    public Action<Enemy> onDisappear;
+
+    // テスト用
+    float _time;
+
+    void Awake()
+    {
+        
+    }
+
+    public void Init()
+    {
+        _time = 0;
+
+        GameObject enemyInstance = Instantiate(_enemyPrefab);
+        _enemy = enemyInstance.GetComponent<Enemy>();
+        _enemy.SetActive(false);
+        _enemy.Init();
+    }
+
+    public void Update()
+    {
+        // テスト用
+        _time += Time.deltaTime;
+        if (_time >= _appearSeconds)
+        {
+            switch (_enemy.CurrentState)
+            {
+                case Enemy.State.Appear: Disappear(); break;
+                case Enemy.State.Disappear: Appear(); break;
+                default: break;
+            }
+            _time = 0;
+        }
+    }
+
+    void Appear()
+    {
+        _enemy.Appear(() => onAppear?.Invoke(_enemy));
+    }
+
+    void Disappear()
+    {
+        _enemy.Disappear(() => onDisappear?.Invoke(_enemy));
+    }
+}
