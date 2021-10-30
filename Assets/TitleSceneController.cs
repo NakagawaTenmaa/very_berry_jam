1ee4a5772b64e409efe2123707f1083940d5ffb2
NakagawaTenmaa <tenko1177@gmail.com>
2021-10-31 02:34:25 +0900

    タイトルシーンの仮を追加

---
+++ Assets/TitleSceneController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneController : MonoBehaviour
+{
+    public void ChengePlayScene()
+    {
+        MySceneManager.ChangePlayeScene();
+    }
+}
